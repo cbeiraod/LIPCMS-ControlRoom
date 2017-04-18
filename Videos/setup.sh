8d7df5aef2a7aabf8a5e0cd4d58dd7d6a99fef0e
Cristóvão Beirão da Cruz e Silva <cbeiraod@lip.pt>
2017-04-18 14:11:28 +0100

    Added the base page for showing the videos

---
+++ Videos/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "Downloading video files, saving download log to download_log.txt, check for any errors"
+rm download_log.txt
+
+#https://cds.cern.ch/record/2147977
+wget -a download_log.txt -O Preparing.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CERN/2016/CERN-MOVIE-2016-025/CERN-MOVIE-2016-025-001/CERN-MOVIE-2016-025-001-5872-kbps-1920x1080-audio-128-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/1973009
+wget -a download_log.txt -O Opening.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CERN/2014/CERN-MOVIE-2014-061/CERN-MOVIE-2014-061-001/CERN-MOVIE-2014-061-001-6000-kbps-1920x1080-audio-128-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/1753412
+wget -a download_log.txt -O CMSMoving.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CERN/2014/CERN-MOVIE-2014-038/CERN-MOVIE-2014-038-001/CERN-MOVIE-2014-038-001-5472-kbps-1620x1080-audio-128-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/1600818
+wget -a download_log.txt -O MuonInstallation.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CERN/2013/CERN-MOVIE-2013-113/CERN-MOVIE-2013-113-001/CERN-MOVIE-2013-113-001-5872-kbps-1920x1080-audio-128-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/1556343
+wget -a download_log.txt -O BeampipeExtraction.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CERN/2013/CERN-MOVIE-2013-073/CERN-MOVIE-2013-073-001/CERN-MOVIE-2013-073-001-5872-kbps-1920x1080-audio-128-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/1111151
+wget -a download_log.txt -O BeampipeInstallation.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CERN/2008/CERN-MOVIE-2008-026/CERN-MOVIE-2008-026-001/CERN-MOVIE-2008-026-001-5872-kbps-1920x1080-audio-128-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/1121421
+wget -a download_log.txt -O ECALEndcaps.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CERN/2008/CERN-MOVIE-2008-046/CERN-MOVIE-2008-046-001/CERN-MOVIE-2008-046-001-5872-kbps-1920x1080-audio-128-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/1083427
+wget -a download_log.txt -O EndcapYE.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CERN/2008/CERN-MOVIE-2008-002/CERN-MOVIE-2008-002-001/CERN-MOVIE-2008-002-001-5872-kbps-1920x1080-audio-128-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/1052607
+wget -a download_log.txt -O ECALCrystal.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CERN/2007/CERN-MOVIE-2007-008/CERN-MOVIE-2007-008-001/CERN-MOVIE-2007-008-001-5872-kbps-1920x1080-audio-128-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/1019674  - OLD
+wget -a download_log.txt -O CentralCMS.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CERN/2007/CERN-MOVIE-2007-002/CERN-MOVIE-2007-002-001/CERN-MOVIE-2007-002-001-1436-kbps-853x480-audio-64-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/2001543
+wget -a download_log.txt -O PixelInsertion.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CERN/2015/CERN-MOVIE-2015-009/CERN-MOVIE-2015-009-001/CERN-MOVIE-2015-009-001-5872-kbps-1920x1080-audio-128-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/2112102  - OLD
+wget -a download_log.txt -O StructuralConstruction.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CERN/2003/CERN-MOVIE-2003-009/CERN-MOVIE-2003-009-002/CERN-MOVIE-2003-009-002-1136-kbps-640x480-audio-64-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/2126012
+wget -a download_log.txt -O HiggsZZCandidate.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/OPEN/2016/OPEN-MOVIE-2016-002/OPEN-MOVIE-2016-002-001/OPEN-MOVIE-2016-002-001-5872-kbps-1920x1080-audio-128-kbps-stereo.mp4"
+
+#https://cds.cern.ch/record/2021541
+wget -a download_log.txt -O Collision13TeV.mp4 "https://mediaarchive.cern.ch/MediaArchive/Video/Public/Movies/CMS/2015/CMS-MOVIE-2015-006/CMS-MOVIE-2015-006-002/CMS-MOVIE-2015-006-002-5872-kbps-1920x1080-audio-128-kbps-stereo.mp4"
+

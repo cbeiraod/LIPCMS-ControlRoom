5fe2492efed686ed948511e22dfa7d5a5d3a5243
Cristóvão Beirão da Cruz e Silva <CrisXed@gmail.com>
2017-09-07 19:19:14 +0100

    Finished interface

--- LIPCMS_ROC.js
+++ LIPCMS_ROC.js
@@ -90,11 +90,6 @@ var publicVisiblePages = {
   bottomRight: allPages.cosmicMuons,
 };
 
-var chosenPages = [
-  shiftVisiblePages.bottomLeft,
-  shiftVisiblePages.bottomLeftCentre,
-];
-
 var closeOpenedWindows = function()
 {
   var failed = []
@@ -105,6 +100,189 @@ var closeOpenedWindows = function()
       failed.push(openWindows[i]);
   }
   openWindows = failed
+}
+
+var choosePage = function(visiblePages)
+{
+  blockingDiv = document.getElementById('disablingDiv');
+  blockingDiv.style.display = 'block';
+
+  var screenSelection = document.createElement('div');
+  screenSelection.className = 'screenSelector';
+
+  var thisHeader = document.createElement('header')
+  var text = document.createTextNode("You are not running in the Remote Operation Center. Click on the screen that you want to emulate:");
+  thisHeader.appendChild(text);
+  screenSelection.appendChild(thisHeader);
+
+
+
+  var break1 = document.createElement('br');
+  screenSelection.appendChild(break1);
+  var div1 = document.createElement('div');
+  div1.className = "center"
+  screenSelection.appendChild(div1);
+
+  var screen1 = document.createElement('div');
+  screen1.className = "screen";
+  screen1.onclick = function()
+  {
+    openWindow(visiblePages.topLeft.name, visiblePages.topLeft.URL);
+    blockingDiv.removeChild(screenSelection);
+    document.getElementById('disablingDiv').style.display='none';
+  };
+  var text1 = document.createTextNode("1");
+  screen1.appendChild(text1);
+  div1.appendChild(screen1);
+
+  var screen2 = document.createElement('div');
+  screen2.className = "screen";
+  screen2.onclick = function()
+  {
+    openWindow(visiblePages.topCentre.name, visiblePages.topCentre.URL);
+    blockingDiv.removeChild(screenSelection);
+    document.getElementById('disablingDiv').style.display='none';
+  };
+  var text2 = document.createTextNode("2");
+  screen2.appendChild(text2);
+  div1.appendChild(screen2);
+
+  var screen3 = document.createElement('div');
+  screen3.className = "screen";
+  screen3.onclick = function()
+  {
+    openWindow(visiblePages.topRight.name, visiblePages.topRight.URL);
+    blockingDiv.removeChild(screenSelection);
+    document.getElementById('disablingDiv').style.display='none';
+  };
+  var text3 = document.createTextNode("3");
+  screen3.appendChild(text3);
+  div1.appendChild(screen3);
+
+
+
+  var break2 = document.createElement('br');
+  screenSelection.appendChild(break2);
+  var div2 = document.createElement('div');
+  div2.className = "center"
+  screenSelection.appendChild(div2);
+
+  var screen4 = document.createElement('div');
+  screen4.className = "screen";
+  screen4.onclick = function()
+  {
+    openWindow(visiblePages.midLeft.name, visiblePages.midLeft.URL);
+    blockingDiv.removeChild(screenSelection);
+    document.getElementById('disablingDiv').style.display='none';
+  };
+  var text4 = document.createTextNode("4");
+  screen4.appendChild(text4);
+  div2.appendChild(screen4);
+
+  var screen5 = document.createElement('div');
+  screen5.className = "screen";
+  screen5.onclick = function()
+  {
+    openWindow(visiblePages.midCentre.name, visiblePages.midCentre.URL);
+    blockingDiv.removeChild(screenSelection);
+    document.getElementById('disablingDiv').style.display='none';
+  };
+  var text5 = document.createTextNode("5");
+  screen5.appendChild(text5);
+  div2.appendChild(screen5);
+
+  var screen6 = document.createElement('div');
+  screen6.className = "screen";
+  screen6.onclick = function()
+  {
+    openWindow(visiblePages.midRight.name, visiblePages.midRight.URL);
+    blockingDiv.removeChild(screenSelection);
+    document.getElementById('disablingDiv').style.display='none';
+  };
+  var text6 = document.createTextNode("6");
+  screen6.appendChild(text6);
+  div2.appendChild(screen6);
+
+
+
+  var break3 = document.createElement('br');
+  screenSelection.appendChild(break3);
+  var div3 = document.createElement('div');
+  div3.className = "center"
+  screenSelection.appendChild(div3);
+
+  var screen7 = document.createElement('div');
+  screen7.className = "screen";
+  screen7.onclick = function()
+  {
+    openWindow(visiblePages.bottomRight.name, visiblePages.bottomRight.URL);
+    blockingDiv.removeChild(screenSelection);
+    document.getElementById('disablingDiv').style.display='none';
+  };
+  var text7 = document.createTextNode("7");
+  screen7.appendChild(text7);
+  div3.appendChild(screen7);
+
+  var screen8 = document.createElement('div');
+  screen8.className = "screen";
+  screen8.onclick = function()
+  {
+    openWindow(visiblePages.bottomRightCentre.name, visiblePages.bottomRightCentre.URL);
+    blockingDiv.removeChild(screenSelection);
+    document.getElementById('disablingDiv').style.display='none';
+  };
+  var text8 = document.createTextNode("8");
+  screen8.appendChild(text8);
+  div3.appendChild(screen8);
+
+  var screen9 = document.createElement('div');
+  screen9.className = "screen";
+  screen9.onclick = function()
+  {
+    openWindow(visiblePages.bottomLeftCentre.name, visiblePages.bottomLeftCentre.URL);
+    blockingDiv.removeChild(screenSelection);
+    document.getElementById('disablingDiv').style.display='none';
+  };
+  var text9 = document.createTextNode("9");
+  screen9.appendChild(text9);
+  div3.appendChild(screen9);
+
+  var screen10 = document.createElement('div');
+  screen10.className = "screen";
+  screen10.onclick = function()
+  {
+    openWindow(visiblePages.bottomLeft.name, visiblePages.bottomLeft.URL);
+    blockingDiv.removeChild(screenSelection);
+    document.getElementById('disablingDiv').style.display='none';
+  };
+  var text10 = document.createTextNode("10");
+  screen10.appendChild(text10);
+  div3.appendChild(screen10);
+
+
+
+  blockingDiv.appendChild(screenSelection);
+
+  return;
+}
+
+var openWindowsOnScreens = function(visiblePages)
+{
+  if(allScreens.length == 1)
+  {
+    choosePage(visiblePages);
+    return;
+  }
+
+  var chosenPages = [
+    visiblePages.bottomLeft,
+    visiblePages.bottomLeftCentre,
+  ];
+
+  for(i = 0; i < allScreens.length; i++)
+  {
+    openWindow(chosenPages[i].name, chosenPages[i].URL, i);
+  }
 }
 
 var closeButton = document.getElementById('closeButton');
@@ -135,5 +313,23 @@ if (wikiButton)
   wikiButton.onclick = function()
   {
     openWindow("LIP-CMS ROC Wiki", "https://github.com/cbeiraod/LIPCMS-OperationCenter/wiki")
+  };
+}
+
+var publicButton = document.getElementById('publicButton');
+if (publicButton)
+{
+  publicButton.onclick = function()
+  {
+    openWindowsOnScreens(publicVisiblePages);
+  };
+}
+
+var shiftButton = document.getElementById('shiftButton');
+if (shiftButton)
+{
+  shiftButton.onclick = function()
+  {
+    openWindowsOnScreens(shiftVisiblePages);
   };
 }

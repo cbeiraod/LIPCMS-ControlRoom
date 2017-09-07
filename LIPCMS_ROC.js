78135d6c9d6e366febc86cfa243fb240be95ec59
Cristóvão Beirão da Cruz e Silva <CrisXed@gmail.com>
2017-09-07 16:30:17 +0100

    Added most pages

--- LIPCMS_ROC.js
+++ LIPCMS_ROC.js
@@ -49,7 +49,51 @@ var allPages = {
   cmsPage0: createPage("https://cmswbm.cern.ch/cmsdb/servlet/PageZero", "CMS Page Zero"),
   cmsPage1: createPage("http://cmspage1.web.cern.ch/cmspage1/", "CMS Page 1"),
   //mycmsPage1: createPage("Pages/CMSStatus.html", "CMS Page 1"),
-}
+  lhcStatus: createPage("https://cmswbm.cern.ch/cmsdb/servlet/LHCStatusDisplay", "LHC Status"),
+  magnetHistory: createPage("https://cmswbm.cern.ch/cmsdb/servlet/MagnetHistory", "Magnet History"),
+  cmsDaq: createPage("http://cmsdoc.cern.ch/B40tvs/cms_daq.html", "CMS DAQ"),
+  onlineDQM: createPage("https://cmsweb.cern.ch/dqm/online", "Online DQM"),
+  runtimeSummary: createPage("https://cmswbm.cern.ch/cmsdb/servlet/FillSummary", "Runtime Summary"),
+  hltRates: createPage("https://cmswbm.cern.ch/cmsdb/servlet/HLTriggerRates", "HLT Rates"),
+  latestStableFill: createPage("https://cmswbm.cern.ch/cmsdb/servlet/FillReport?N=1&BYLASTFILLS=TRUE&STABLE=TRUE", "Latest stable Fill"),
+  liveEvents: createPage("http://cmsdoc.cern.ch/cmscc/cmstv/cmstv.jsp?channel=12&frames=no", "Live Events"),
+  events2009: createPage("http://cmsdoc.cern.ch/cmscc/cmstv/cmstv.jsp?channel=6&frames=no", "Events 2009"),
+  events10_11: createPage("http://cmsdoc.cern.ch/cmscc/cmstv/cmstv.jsp?channel=7&frames=no", "Events 2010-2011"),
+  simEvents: createPage("http://cmsdoc.cern.ch/cmscc/cmstv/cmstv.jsp?channel=8&frames=no", "Event Simulations"),
+  cosmicMuons: createPage("http://cmsdoc.cern.ch/cmscc/cmstv/cmstv.jsp?channel=5&frames=no", "Cosmic Muons"),
+  cmsPhotos: createPage("http://cmsdoc.cern.ch/cmscc/cmstv/cmstv.jsp?channel=9&frames=no", "CMS Photos"),
+};
+
+var shiftVisiblePages = {
+  topLeft: allPages.mylhcPage1,
+  topCentre: allPages.cmsPage0,
+  topRight: allPages.cmsPage1,
+  midLeft: allPages.lhcStatus,
+  midCentre: allPages.magnetHistory,
+  midRight: allPages.cmsDaq,
+  bottomLeft: allPages.onlineDQM,
+  bottomLeftCentre: allPages.runtimeSummary,
+  bottomRightCentre: allPages.hltRates,
+  bottomRight: allPages.latestStableFill,
+};
+
+var publicVisiblePages = {
+  topLeft: allPages.mylhcPage1,
+  topCentre: allPages.liveEvents,
+  topRight: allPages.cmsPage1,
+  midLeft: allPages.events2009,
+  midCentre: allPages.cmsDaq,
+  midRight: allPages.events10_11,
+  bottomLeft: allPages.onlineDQM,
+  bottomLeftCentre: allPages.simEvents,
+  bottomRightCentre: allPages.cmsPhotos,
+  bottomRight: allPages.cosmicMuons,
+};
+
+var chosenPages = [
+  shiftVisiblePages.bottomLeft,
+  shiftVisiblePages.bottomLeftCentre,
+];
 
 var closeOpenedWindows = function()
 {
